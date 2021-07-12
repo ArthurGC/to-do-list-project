e213e98cdb82f626eb8b42453623368af8c5fe1c
arthurGC <aguzmanc@unsa.edu.pe>
2021-07-12 13:02:11 -0500

    Add linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
 
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist',
   },
 
   module: {
@@ -19,11 +19,11 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          "style-loader",
+          'style-loader',
           // Translates CSS into CommonJS
-          "css-loader",
+          'css-loader',
           // Compiles Sass to CSS
-          "sass-loader",
+          'sass-loader',
         ],
       },
     ],
@@ -31,9 +31,9 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
 
-  mode: "development",
+  mode: 'development',
 };
