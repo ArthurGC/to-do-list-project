4d6609a922365ba3653c45e9e4fb60fe25b298a9
arthurGC <aguzmanc@unsa.edu.pe>
2021-07-12 15:15:18 -0500

    Fix indentation

--- src/index.js
+++ src/index.js
@@ -1,35 +1,50 @@
-import "./styles.scss";
+import './styles.scss';
 
 const listContainer = document.querySelector('.container-list');
 
-let toDoTasks = [
+const toDoTasks = [
   {
-    description: "Wash the dishes",
+    description: 'Wash the dishes',
     completed: false,
     index: 0,
   },
   {
-    description: "Brush your teeth",
-    completed: false,
+    description: 'Brush your teeth',
+    completed: true,
     index: 1,
+  },
+  {
+    description: 'Wake up early',
+    completed: false,
+    index: 2,
   },
 ];
 
 const createItemTask = (task) => {
+  if (task.completed === false) {
     listContainer.insertAdjacentHTML('beforeend',
-    `<li>
-    <input type="checkbox" name="task${task.index}" value="task${task.index}" checked=${task.completed}>
+      `<li>
+    <input type="checkbox" name="task${task.index}" value="task${task.index}">
     <label class="text-task">${task.description}</label><br>
     </li>
-    `)
-}
+    `);
+  } else {
+    listContainer.insertAdjacentHTML(
+      'beforeend',
+      `<li>
+    <input type="checkbox" name="task${task.index}" value="task${task.index}" checked>
+    <label class="text-task">${task.description}</label><br>
+    </li>
+    `,
+    );
+  }
+};
 
 const displayTasks = () => {
-    listContainer.innerHTML = '';
-    toDoTasks.forEach(task => createItemTask(task));
-}
+  listContainer.innerHTML = '';
+  toDoTasks.forEach((task) => createItemTask(task));
+};
 
 document.addEventListener('DOMContentLoaded', () => {
-    displayTasks();
+  displayTasks();
 });
-
