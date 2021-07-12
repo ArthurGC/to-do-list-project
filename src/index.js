49d7f0cb54f179f630b37ea7fc81130b72f616ee
arthurGC <aguzmanc@unsa.edu.pe>
2021-07-12 13:31:38 -0500

    Add array of toDoTasks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import './styles.scss';
+import "./styles.scss";
+
+let toDoTasks = [
+  {
+    description: "Wash the dishes",
+    completed: false,
+    index: 1,
+  },
+  {
+    description: "Brush your teeth",
+    completed: false,
+    index: 2,
+  },
+];
