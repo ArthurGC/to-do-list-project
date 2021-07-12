8bdd680d260ac3cc6f16f6ed7f3a645b7c41a39f
arthurGC <aguzmanc@unsa.edu.pe>
2021-07-12 13:57:49 -0500

    Add displayTask function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import "./styles.scss";
 
+const listContainer = document.querySelector('.container-list');
+
 let toDoTasks = [
   {
     description: "Wash the dishes",
     completed: false,
-    index: 1,
+    index: 0,
   },
   {
     description: "Brush your teeth",
     completed: false,
-    index: 2,
+    index: 1,
   },
 ];
+
+const createItemTask = (task) => {
+    listContainer.insertAdjacentHTML('beforeend',
+    `<li>
+    <input type="checkbox" name="task${task.index}" value="task${task.index}" checked=${task.completed}>
+    <label class="text-task">${task.description}</label><br>
+    </li>
+    `)
+}
+
+const displayTasks = () => {
+    listContainer.innerHTML = '';
+    toDoTasks.forEach(task => createItemTask(task));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    displayTasks();
+});
+
