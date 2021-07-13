8d3d4b8839a3858f1bc4eb76000a4cbea3391b49
arthurGC <aguzmanc@unsa.edu.pe>
2021-07-13 12:29:41 -0500

    Add localstorage functions

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import './styles.scss';
+import "./styles.scss";
 
-import {listContainer, createItemTask} from './modules/renderTasks.js'
-import {refrestTargetDragDrop} from './modules/dragDrop.js'
+import { listContainer, createItemTask } from "./modules/renderTasks.js";
+import { refrestTargetDragDrop } from "./modules/dragDrop.js";
+import {checkBoxStatus} from './modules/checkStatus.js';
 
 const toDoTasks = [
   {
-    description: 'Wash the dishes',
+    description: "Wash the dishes",
     completed: false,
     index: 0,
   },
   {
-    description: 'Brush your teeth',
+    description: "Brush your teeth",
     completed: true,
     index: 1,
   },
   {
-    description: 'Wake up early',
+    description: "Wake up early",
     completed: false,
     index: 2,
   },
@@ -26,14 +27,34 @@ const displayTasks = () => {
   toDoTasks.forEach((task) => createItemTask(task));
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+const refrestCompletedTask = (bool,element) => {
+  if (bool) {
+      toDoTasks[parseInt(element.dataset.id)].completed = true;
+  } else {
+    toDoTasks[parseInt(element.dataset.id)].completed = false;
+  }
+}
+
+const getTaskData = () => {
+  if (localStorage.getItem('TaskData') != null) {
+    toDoTasks = JSON.parse(localStorage.getItem('TaskData'));
+  }
+};
+
+const setData = () => {
+  localStorage.setItem('TaskData', JSON.stringify(toDoTasks));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  getTaskData();
   displayTasks();
   refrestTargetDragDrop();
 });
 
-// listContainer.addEventListener('click', (e) => {
-
-//   if (e.target.classList.contains('icon-item')) {
-
-//   }
-// })
+listContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("checkbox")) {
+    checkBoxStatus(e.target);
+    refrestCompletedTask(e.target.checked, e.target);
+    setData();
+  }
+});
