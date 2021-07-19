6ea90b5d8cff621243b7a31e4238cb7dea97b4e3
arthurGC <aguzmanc@unsa.edu.pe>
2021-07-19 17:34:51 -0500

    Add a bundle of repetitive functions

--- src/modules/remove.js
+++ src/modules/remove.js
@@ -1,19 +1,23 @@
-import { setDataLocalStorage, getDataLocalStorage } from './store.js';
-import { refreshIndex, renderTaskDom } from './render.js';
-import { refreshDragDropTarget } from './drag-drop.js';
-import { refreshDescriptions } from './description.js';
+import { setDataLocalStorage, getDataLocalStorage } from "./store.js";
+import { refreshIndex, renderTaskDom } from "./render.js";
+import { refreshDragDropTarget } from "./drag-drop.js";
+import { refreshDescriptions } from "./description.js";
+
+const bundleRefreshHandlersAndUpdate = (listOfTask) => {
+  refreshIndex(listOfTask);
+  setDataLocalStorage(listOfTask);
+  renderTaskDom();
+  refreshDragDropTarget();
+  refreshDescriptions();
+};
 
 export const removeTask = (event) => {
-  let isRemoveIcon = event.target.classList.contains('remove');
+  let isRemoveIcon = event.target.classList.contains("remove");
   if (isRemoveIcon) {
     const listTasks = getDataLocalStorage();
     const id = parseInt(event.target.parentElement.dataset.id, 10);
     listTasks.splice(id, 1);
-    refreshIndex(listTasks);
-    setDataLocalStorage(listTasks);
-    renderTaskDom();
-    refreshDragDropTarget();
-    refreshDescriptions();
+    bundleRefreshHandlersAndUpdate(listTasks);
   }
 };
 
@@ -21,11 +25,7 @@ export const removeTaskChecked = () => {
   const listTasks = getDataLocalStorage();
   if (listTasks !== []) {
     const newListTask = listTasks.filter((task) => task.status === false);
-    refreshIndex(newListTask);
-    setDataLocalStorage(newListTask);
-    renderTaskDom();
-    refreshDragDropTarget();
-    refreshDescriptions();
+    bundleRefreshHandlersAndUpdate(newListTask);
   }
 };
 
