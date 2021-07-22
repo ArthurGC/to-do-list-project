934490d316b5ac4cdf618ecba2941e3f558493f0
arthurGC <aguzmanc@unsa.edu.pe>
2021-07-21 22:15:51 -0500

    Add test to removeCompletedTasks function

--- src/utilities/second-test.test.js
+++ src/utilities/second-test.test.js
@@ -29,8 +29,7 @@ describe("Update checkbox and task status", () => {
   let checkBox = document.querySelector(".checkbox");
 
   const getStatusCheckBoxDom = () => {
-    let checkBoxDom = document.querySelector(".checkbox");
-    return checkBoxDom.getAttribute("checked");
+    return checkBox.checked;
   };
 
   // Act --------------------------------------------------------------------------->
@@ -66,5 +65,99 @@ describe("Update checkbox and task status", () => {
 
   test("change checkbox status to true in DOM", () => {
     expect(getStatusCheckBoxDom()).toBeTruthy();
+  });
+  
+});
+
+describe("Test remove completed task", () => {
+  // Arrange --------------------------------------------------------------------------->
+  document.body.innerHTML =
+    "<div>" +
+    '<ul class="container-list">' +
+    '<li data-id="0" class="item">' +
+    '<input class="checkBox" type="checkbox">' +
+    '<label class="text-task" contenteditable=true>Do the laundry</label>' +
+    '<i class="fas fa-trash-alt remove"></i>' +
+    "</li>" +
+    '<li data-id="1" class="item">' +
+    '<input class="checkBox" type="checkbox" checked="true">' +
+    '<label class="text-task" contenteditable=true>Wash my teeth</label>' +
+    '<i class="fas fa-trash-alt remove"></i>' +
+    "</li>" +
+    '<li data-id="2" class="item">' +
+    '<input class="checkBox" type="checkbox" checked="true">' +
+    '<label class="text-task" contenteditable=true>Clean my house</label>' +
+    '<i class="fas fa-trash-alt remove"></i>' +
+    "</li>" +
+    "</ul>" +
+    "</div>";
+
+  const localStorage = new LocalStorage();
+  let listTask = [
+    {
+      description: "Do the laundry",
+      status: false,
+      index: 0,
+    },
+    {
+      description: "Wash my teeth",
+      status: true,
+      index: 1,
+    },
+    {
+      description: "Clean my house",
+      status: true,
+      index: 2,
+    },
+  ];
+  localStorage.setDataLocalStorage(listTask);
+
+  const bundleRefreshHandlersAndUpdate = (list) => {
+    localStorage.setDataLocalStorage(list);
+
+    const firstArrayIndex = [];
+    const secondArrayIndex = [];
+    for (let i = 0; i < listTask.length; i += 1) {
+        firstArrayIndex.push(i);
+    }
+    list.forEach((element) => secondArrayIndex.push(element.index));
+
+    const difference = firstArrayIndex.filter((x) => secondArrayIndex.indexOf(x) === -1);
+    difference.forEach(index => {
+        let identifier = `li[data-id="${index}"]`;
+        document.querySelector(identifier).remove();
+    })
+  }
+
+  const getTaskDomLength = () => {
+    let tasksDom = [...document.querySelectorAll(".item")];
+    return tasksDom.length;
+  };
+
+  // Act --------------------------------------------------------------------------->
+  const removeTaskChecked = () => {
+    const listTasks = localStorage.getDataLocalStorage();
+    if (listTasks !== []) {
+      const newListTask = listTasks.filter((task) => task.status === false);
+      bundleRefreshHandlersAndUpdate(newListTask);
+    }
+  };
+
+  // Assert --------------------------------------------------------------------------->
+
+  removeTaskChecked();
+
+  test("remove completed tasks from localStorage", () => {
+    expect(localStorage.getDataLocalStorage()).toEqual([
+      {
+        description: "Do the laundry",
+        status: false,
+        index: 0,
+      },
+    ]);
+  });
+
+  test("change checkbox status to true in DOM", () => {
+    expect(getTaskDomLength()).toBe(1);
   });
 });
